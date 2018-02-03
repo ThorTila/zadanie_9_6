a89a68df9ada7c3667b5880c3baa23a8a6c6f374
Thortilla <33997787+ThorTila@users.noreply.github.com>
2018-02-03 23:57:33 +0100

    fix

--- js/script.js
+++ js/script.js
@@ -2,6 +2,6 @@ var button = document.getElementById('addElem');
 var list = document.getElementById('list');
 button.addEventListener('click', function() {
   var newElement = document.createElement('li');
-  newElement.innerText = 'item ' + document.getElementsByTagName('li').length;
+  newElement.innerText = 'item ' + list.getElementsByTagName('li').length;
   list.appendChild(newElement);
 });
